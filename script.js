ee62dd25e1baade5990d978e92b2335e03359611
Samuel do nascimento ramos <hmsrp.ramos7@gmail.com>
2024-10-14 16:48:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var numero1 = parseInt (prompt("digirte um numero"))
+var numero2 = parseInt (prompt("digirte um numero"))
+var resultado = numero1 + numero2
+alert ("faça a soma desses numeros" + resultado)
